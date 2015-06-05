8b6639ed0824f3f62a273ec6874c0902426c62e7
Tytan <quentin.chateau@gmail.com>
2015-06-05 15:46:04 +0200

    2 taches RT

--- TP3/ex2/src/helloworld2RT.c
+++ TP3/ex2/src/helloworld2RT.c
@@ -1,25 +1,49 @@
 #include <sys/mman.h>
 #include <native/task.h>
+#include <native/sem.h>
 
 #include <unistd.h>
 
-#define TASK1_PRIO  9 
-#define TASK2_PRIO  99 
+#define TASK_PRIO  99 
 #define TASK_MODE  0  /* No flags */
 #define TASK_STKSZ 0  /* Stack size (use default one) */
 
+RT_SEM sem[2];
 RT_TASK task1, task2;
+
 void task1_body (void *cookie) {
-	for (;;) {
+	int i, err;
+	RT_SEM *sem;
+	sem = (RT_SEM*) cookie;
+	for (i=0;i<10;i++) {
+		err = rt_sem_p(&sem[0], TM_INFINITE);
+		if (err) {
+			printf("Failed to acquire sem 0 : %i\n", err);
+		}
 		printf("Hello ");
-		rt_task_sleep(100000000);
+		rt_task_sleep(100000);
+		err = rt_sem_v(&sem[1]);
+		if (err) {
+			printf("Failed to release sem 1 : %i\n", err);
+		}
 	}
 }
 
 void task2_body (void *cookie) {
-	for (;;) {
+	int i, err;
+	RT_SEM *sem;
+	sem = (RT_SEM*) cookie;
+	for (i=0;i<10;i++) {
+		err = rt_sem_p(&sem[1], TM_INFINITE);
+		if (err) {
+			printf("Failed to acquire sem 1 : %i\n", err);
+		}
 		printf("World RT !\n");
-		rt_task_sleep(100000000);
+		rt_task_sleep(100000);
+		err = rt_sem_v(&sem[0]);
+		if (err) {
+			printf("Failed to release sem 0 : %i\n", err);
+		}
 	}
 }
 
@@ -29,19 +53,23 @@ void cleanup (void) {
 }
 
 int main () {
-	int err1, err2;
+	int err, err1, err2;
 	mlockall(MCL_CURRENT|MCL_FUTURE);
-	err1 = rt_task_create(&task1, "Hello", TASK_STKSZ, TASK1_PRIO, TASK_MODE);
-	err2 = rt_task_create(&task2, "WorldRT", TASK_STKSZ, TASK2_PRIO, TASK_MODE);
+	err = rt_sem_create(&sem[0], "semHello", 1, S_FIFO);
+	if (err) {
+		printf("Failed to create sem 0 : %i\n", err);
+	}
+	err = rt_sem_create(&sem[1], "semWorld", 0, S_FIFO);
+	if (err) {
+		printf("Failed to create sem 1 : %i\n", err);
+	}
+	err1 = rt_task_create(&task1, "Hello", TASK_STKSZ, TASK_PRIO, TASK_MODE);
+	err2 = rt_task_create(&task2, "WorldRT", TASK_STKSZ, TASK_PRIO, TASK_MODE);
 	if (!err1)
-		rt_task_start(&task1,&task1_body,NULL);
+		rt_task_start(&task1,&task1_body,sem);
 	if (!err2)
-		rt_task_start(&task2,&task2_body,NULL);
-	sleep(3);
-	cleanup();
-	printf("Cleaned up task\n");
-	sleep(3);
-	printf("Exit\n");
+		rt_task_start(&task2,&task2_body,sem);
+	sleep(5);
 	return 0;
 }
 
